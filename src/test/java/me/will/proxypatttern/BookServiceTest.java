f2349b18fab30220736195c3af90b73f49faf0ce
seungh0 <will.seungho@gmail.com>
2020-06-17 15:17:20 +0900

    Dynamic Proxy (2)

--- src/test/java/me/will/proxypatttern/BookServiceTest.java
+++ src/test/java/me/will/proxypatttern/BookServiceTest.java
@@ -6,6 +6,15 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+/**
+ * Dynamic Proxy
+ *
+ * 매번 클래스를 생성해야하는 문제점은 해결할 수 있다.
+ * but) InvocationHandler() 코드가 지저분해질 수 있음.
+ *
+ * => 그래서 나온것이 Spring AOP
+ */
+
 class BookServiceTest {
 
 	private BookService bookServiceProxy = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class}, new InvocationHandler() {
@@ -14,8 +23,21 @@ class BookServiceTest {
 
 		@Override
 		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-			return method.invoke(bookService, args); // DefaultBookService만 하는 역할
+			// rent()에만 적용하고 싶다면
+			if (!method.getName().equals("rent")) {
+				return method.invoke(bookService, args);
+			}
+
+			System.out.println("aaaa"); // 부가기능 추가
+			Object invoke = method.invoke(bookService, args);
+			System.out.println("bbbb"); // 부가 기능 추가
+			return invoke;
 		}
+
+//		@Override
+//		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//			return method.invoke(bookService, args); // DefaultBookService만 하는 역할
+//		}
 	});
 
 	@Test
