d3b48209d6e460ec075084d4b349d5b12c8ce198
seungh0 <will.seungho@gmail.com>
2020-06-17 15:13:58 +0900

    Dynamic Proxy (1)

--- src/test/java/me/will/proxypatttern/BookServiceTest.java
+++ src/test/java/me/will/proxypatttern/BookServiceTest.java
@@ -2,9 +2,21 @@ package me.will.proxypatttern;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 class BookServiceTest {
 
-	private BookService bookServiceProxy  = new BookServiceProxy(new DefaultBookService());
+	private BookService bookServiceProxy = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class}, new InvocationHandler() {
+
+		BookService bookService = new DefaultBookService();
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			return method.invoke(bookService, args); // DefaultBookService만 하는 역할
+		}
+	});
 
 	@Test
 	void rent() {
